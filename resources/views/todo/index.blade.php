b5a7690a244871572b2be91c98e092358f3f1975
Maitreya-22 <55971904+Maitreya-22@users.noreply.github.com>
2021-04-19 19:52:07 +0530

    Update index.blade.php

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -4,27 +4,48 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/app.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <title>TODO | Home</title>
 </head>
+<style>
+    li{
+        list-style-type:none;
+    }
+</style>
+
 <body>
-    <h1>All your TODOsss...</h1>
+   
+    
+
+    <div class="wrapper-grid">
+            <div class="container-grid">
+                <h1>
+                    TODO APP
+                </h1><hr>
+            <h1>ALL YOUR TODOS</h1>
     <h3>
-        <a href="/create">Create</a>
+        <a href="/create" class="create">CREATE</a>
     </h3>
+    <hr>
     <h3>
         <x-alert />
     </h3>
     @foreach($todos as $todo)
-        <li>
+        <li class="litag">
             @if($todo->completed)
-                <span style="text-decoration:line-through">{{$todo->title}}</span>
+                <span style="text-decoration:line-through red; color:white !important;">{{$todo->title}}</span>
             @else 
                 {{$todo->title}}
             @endif
-            <a href="{{asset('/' . $todo->id . '/edit')}}">Edit</a>
-            <a href="{{asset('/' . $todo->id . '/completed')}}">Completed</a>
-            <a href="{{asset('/' . $todo->id . '/delete')}}">Delete</a>
-        </li>
+            <br><br>  <a href="{{asset('/' . $todo->id . '/edit')}}" class="ecd">EDIT</a>&nbsp&nbsp&nbsp
+            <a href="{{asset('/' . $todo->id . '/completed')}}" class="ecd">COMPLETED</a>&nbsp&nbsp&nbsp
+            <a href="{{asset('/' . $todo->id . '/delete')}}" class="ecd">DELETE</a>
+        </li><br><br><hr>
     @endforeach
+                <!-- <button class="btn-new">Add to cart</button> -->
+        </div>
 </body>
 </html>
