f00587b4eb71785a23e7b4f1acdf8c6d730949f1
Maitreya-22 <55971904+Maitreya-22@users.noreply.github.com>
2021-04-19 19:51:16 +0530

    Update create.blade.php

--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -4,19 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/app.css">
     <title>TODO | Create</title>
 </head>
 <body style="text-align:center">
-    <h1>Create your TODO</h1>
+<div class="wrapper-grid">
+            <div class="container-grid">
+    <h1>CREATE YOUR TODO</h1>
     <h3>
         <x-alert />
     </h3>
     <form action="/upload" method="post">
         @csrf
-        <input type="text" name="title" />
-        <input type="submit" value="Create" />
+        <input type="text" class="text-edit" name="title" /> <br><br>
+        <input type="submit" class="edit-btn" value="Create" /><br><br>
     </form>
     <br>
-    <a href="/index">Back</a>
+    <a href="/index" class="back-btn" >Back</a> <br><br><br><br>
+    </div>
+    </div>
 </body>
 </html>
